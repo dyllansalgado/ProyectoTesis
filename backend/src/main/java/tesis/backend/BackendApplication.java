d60db3912880217c9edde2b23a033c0b1b236f8c
Dyllan <dyllan.salgado@usach.cl>
2022-09-13 13:19:00 -0300

    Correccion backend

--- backend/src/main/java/tesis/backend/BackendApplication.java
+++ backend/src/main/java/tesis/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package tesi.backend;
+package tesis.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/tesis/backend/BackendApplicationTests.java
+++ backend/src/test/java/tesis/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package tesi.backend;
+package tesis.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
